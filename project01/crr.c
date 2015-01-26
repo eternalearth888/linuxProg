74c30ab3250adb60cdf0bf14a7cd77ba708b150c
Maria Deslis <mdeslis@gmail.com>
2015-01-26 15:57:19 -0700

    able to read and store arbitrary number of rooms from file

--- project01/crr.c
+++ project01/crr.c
@@ -12,8 +12,7 @@ struct room makeroom(int id, char* name) {
 
 int main(int argc, char* argv[]) {
 	// Struct Related
-	#define FLOORPLAN 10
-	struct room floorplan[FLOORPLAN];
+	struct room *floorplan;
 
 	// File Related
 	FILE *ifp;
@@ -25,25 +24,46 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
+	// File error check
 	if ( ifp == NULL) {
 		fprintf(stderr, "Can't open input file: '%s'\n", argv[1]);
 		exit(1);
-	} 
+	}
+
+	// Count number of non-blank lines in rooms.dat
+	char line;
+	int numRooms = 0;
+
+	while((line = fgetc(ifp)) != EOF) {
+		if (line == '\n') {
+			numRooms++;
+		}
+	}
+
+	//Start at the beginning of the file now that we have reached the end
+	fseek(ifp, 0, SEEK_SET);
+
+	// Now that we know how many rooms to get from the file
+	// Malloc the struct to adjust as necessary	
+	floorplan = malloc(numRooms*sizeof(struct room));
 
 	// READ argv[1] text file (ex: rooms.dat)
+	// Input values for room names into struct and add ids with them
 	int i = 0;
 	while (fscanf(ifp, "%s", &floorplan[i].name) != EOF) {
 		floorplan[i].id = i;
 		i++;
 	}
 
-	// Verify rooms
-	for (int j = 0; j < FLOORPLAN; j++) {
+	printf("Num Rooms: %i\n",numRooms);
+
+	// Verify ID:ROOMNAME
+	for (int j = 0; j < numRooms; j++) {
 		printf("%i:%s\n", floorplan[j].id,floorplan[j].name);
 	}
 
 	fclose(ifp);
-	//free(floorplan);
+	free(floorplan);
 
 	return 0;
 }
